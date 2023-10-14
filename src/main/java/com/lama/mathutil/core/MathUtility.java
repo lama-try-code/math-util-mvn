99bfd4d61ef12c6119526484b6e2daa859a021db
lama-try-code <dotranlam2003@gmail.com>
2023-10-14 16:50:26 +0700

    update getf() to use recursion

--- src/main/java/com/lama/mathutil/core/MathUtility.java
+++ src/main/java/com/lama/mathutil/core/MathUtility.java
@@ -18,19 +18,35 @@ public class MathUtility {
 
     //n < 0 && n > 20 k tính, vì 21! tràn kiểu long
     //0! = 1! = 1
+//    public static long getFactorial(int n) {
+//        long result = 10;
+//
+//        if (n < 0 || n > 20)
+//            throw new IllegalArgumentException("Invalid number. Number must be between 0 to 20.");
+//            
+//        if (n == 0 || n == 1)
+//            return 1;
+//
+//        for (int i = 2; i <= n; i++)
+//            result *= i;
+//        
+//        return result;
+//    }
+    //upgrade getFactorial, using recursion
+    //đệ quy = gọi lại chính mình với 1 quy mô nhỏ hơn
     public static long getFactorial(int n) {
-        long result = 10;
-
-        if (n < 0 || n > 20)
+        if (n < 0 || n > 20) {
             throw new IllegalArgumentException("Invalid number. Number must be between 0 to 20.");
-            
-        if (n == 0 || n == 1)
+        }
+
+        if (n == 0 || n == 1) {
             return 1;
+        }
 
-        for (int i = 2; i <= n; i++)
-            result *= i;
-        
-        return result;
+        return n * getFactorial(n - 1);
     }
-
+    //Việc tối ưu này đúng hay sai về chất lượng code
+    //=> leader chỉ cần nhìn màu xanh là được vì nếu làm tốt => phải màu xanh, không care chi tiết
+//    kĩ thuật xanh => test xanh => không cần nhìn chi tiết
+//    người ta gọi là Regression test => test hồi quy
 }
